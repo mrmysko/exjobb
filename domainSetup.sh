59dd1d044c96a3ad922079953e1dd0441b255879
MrMysko <orkar_-.-@hotmail.com>
2024-11-23 00:16:19 +0100

    feat: error handling and realmd heredoc

--- domainSetup.sh
+++ domainSetup.sh
@@ -5,11 +5,13 @@
 
 # Usage: sudo bash -c "${wget -qLO - https://raw.githubusercontent.com/mrmysko/exjobb/refs/heads/main/domainSetup.sh}"
 
-client=True
-domain_user="Administrator"
-domain="Labb.se"
-permit_group="Domain Users"
-permit_admin="Domain Admins"
+CLIENT=true
+DOMAIN_USER="Administrator"
+DOMAIN="Labb.se"
+PERMIT_GROUP="Domain Users"
+PERMIT_ADMIN="Domain Admins"
+NAME=$(lsb_release -a 2>/dev/null | grep "Distributor ID:" | cut -f2)
+VERSION=$(lsb_release -a 2>/dev/null | grep "Release:" | cut -f2)
 
 msg_info() {
     echo "[INFO] $1"
@@ -28,30 +30,51 @@ fi
 msg_info "Setting up client"
 
 msg_info "Installing dependencies"
-apt update && apt install -y realmd sssd sssd-tools libnss-sss adcli
+if ! apt update && apt install -y realmd sssd sssd-tools libnss-sss adcli; then
+    msg_info "Failed to install dependencies"
+    exit 1
+fi
+
+cat <<EOF >/etc/realmd.conf
+
+[active-directory]
+default-client = sssd
+os-name = $NAME
+os-version = $VERSION
+
+[users]
+default-home = /home/%D/%U
+
+[$DOMAIN]
+fully-qualified-names = Off
+
+EOF
 
 msg_info "Joining domain."
-realm join -U "${domain_user}" "${domain}"
+if ! realm join -U "$DOMAIN_USER" "$DOMAIN"; then
+    msg_info "Failed to join domain"
+    exit 1
+fi
 
-msg_info "Change sssd conf"
-sed -i 's/fallback_homedir = .*/fallback_homedir = \/home\/%u/' /etc/sssd/sssd.conf
-sed -i 's/use_fully_qualified_names = .*/use_fully_qualified_names = False/' /etc/sssd/sssd.conf
-systemctl restart sssd
+#msg_info "Change sssd conf"
+#sed -i 's/fallback_homedir = .*/fallback_homedir = \/home\/%u/' /etc/sssd/sssd.conf
+#sed -i 's/use_fully_qualified_names = .*/use_fully_qualified_names = False/' /etc/sssd/sssd.conf
+#systemctl restart sssd
 
 msg_info "Enable pam_mkhomedir"
 pam-auth-update --enable mkhomedir
 
 msg_info "Changing login permissions"
 realm deny --all
-realm permit -g "${permit_admin}"
+realm permit -g "$PERMIT_ADMIN"
 
-if [[ ${client} ]]; then
-    realm permit -g "${permit_group}"
+if [[ $CLIENT ]]; then
+    realm permit -g "$PERMIT_GROUP"
 fi
 
 #msg_info "Giving sudo to admins"
 #SUDOERS_TEMP=$(mktemp)
-#echo "%$(escape_spaces "${permit_admin}") ALL=(ALL) ALL" >"$SUDOERS_TEMP"
+#echo "%$(escape_spaces "${PERMIT_ADMIN}") ALL=(ALL) ALL" >"$SUDOERS_TEMP"
 #visudo --check --quiet "$SUDOERS_TEMP" && mv "$SUDOERS_TEMP" /etc/sudoers.d/domain_sudoers
 
 msg_info "Rebooting in 5 seconds..."
