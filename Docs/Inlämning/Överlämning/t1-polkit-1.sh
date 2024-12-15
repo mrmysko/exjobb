0988952a26104b264e87e3f13b4bc7187ef37b92
MrMysko <orkar_-.-@hotmail.com>
2024-12-15 17:54:18 +0100

    fix: merge directories

--- Docs/Inlämning/Överlämning/t1-polkit-1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cat <<'EOF' >/etc/polkit-1/rules.d/49-ubuntu-admin.rules
-polkit.addAdminRule(function(action, subject) {
-	return ["unix-group:T1_Admins@DD.COM"];
-});
-EOF
--- Docs/Inlämning/Överlämning/tb-polkit-1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cat <<'EOF' >/etc/polkit-1/rules.d/49-ubuntu-admin.rules
-polkit.addAdminRule(function(action, subject) {
-	return ["unix-group:TB_Admins@DD.COm"];
-});
-EOF
